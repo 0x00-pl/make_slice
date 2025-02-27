4b0af198046618ab4c93e20601105f67927ffc6f
pl <0x00.pl@gmail.com>
2025-02-28 03:06:00 +0800

    modified:   tests/__init__.py 	modified:   tests/test_make_slice.py

--- tests/test_make_slice.py
+++ tests/test_make_slice.py
@@ -1,13 +1,17 @@
+"""test for make_slice.py."""
+
 from make_slice import MakeSlice, make_slice
 
 
-def test_make_slice_class():
+def test_make_slice_class() -> None:
+    """Test the MakeSlice class."""
     data: list[int] = list(range(10))
     s: slice = MakeSlice()[::-1]
     assert data[s] == data[::-1]
 
 
-def test_make_slice_object():
+def test_make_slice_object() -> None:
+    """Test the make_slice object."""
     data: list[int] = list(range(10))
     s: slice = make_slice[::-1]
     assert data[s] == data[::-1]
