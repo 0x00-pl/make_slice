e4665caeaab731011c8911671466115336985152
pl <0x00.pl@gmail.com>
2025-03-08 03:24:42 +0800

    modified:   make_slice/__init__.py

--- make_slice/__init__.py
+++ make_slice/__init__.py
@@ -1,12 +1,10 @@
 """A module that provides a class to mimic slice behavior."""
 
-from typing import Self
-
 
 class MakeSlice:
     """A class that returns the item itself when called, mimicking a slice object."""
 
-    def __getitem__(self: Self, item: slice) -> slice:
+    def __getitem__(self, item: slice) -> slice:
         """Mimics the behavior of a slice object by returning the item itself."""
         return item
 
